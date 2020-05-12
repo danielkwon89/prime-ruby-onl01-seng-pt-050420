c9b7f4a44161f79e725b0d1477bd27bc31f816bd
Daniel Kwon <danielkwon89@gmail.com>
2020-05-12 00:02:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,9 @@ def prime?(number)
 
   if number == -1 || 0 || 1
     return false
+    
+  elsif number < 0
+    return false
 
   elsif number > 0
     (2..(number - 1)).each do |i| 
@@ -9,15 +12,6 @@ def prime?(number)
         return false 
       end #nested if statement
     end #each iterator
-
-  elsif number < 0
-    ((number + 1)..-2).each do |i|
-      if number % i == 0
-        return false 
-      end #nested if statement
-    end #each iterator
-
   end #outer if statement
-
   true
 end #prime? method
